ac937e11a3bfdd75330f12190d22eff5f7ee5015
ValeedAnjum <allinone644@gmail.com>
2020-10-20 18:12:43 +0500

    first frontend MVP

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -9,10 +9,12 @@ import {
   Tab,
   Paper,
   Grid,
+  CssBaseline,
 } from "@material-ui/core";
 const useStyles = makeStyles(() => ({
   appbar: {
     backgroundColor: "transparent",
+    // opacity: "0.5",
   },
   title: {
     flexGrow: "1",
@@ -21,7 +23,7 @@ const useStyles = makeStyles(() => ({
     height: "80vh",
     backgroundSize: "cover",
     backgroundImage:
-      "url(https://images.pexels.com/photos/592753/pexels-photo-592753.jpeg?cs=srgb&dl=pexels-aaditya-arora-592753.jpg&fm=jpg)",
+      "url(https://images.pexels.com/photos/705164/computer-laptop-work-place-camera-705164.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)",
   },
   tabs: {
     "& > .MuiTabs-scroller": {
@@ -31,11 +33,17 @@ const useStyles = makeStyles(() => ({
     },
   },
 }));
+
+const pictures = [
+  "https://images.pexels.com/photos/5425971/pexels-photo-5425971.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/5560026/pexels-photo-5560026.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/876466/pexels-photo-876466.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+];
 const Navbar = () => {
   const classes = useStyles();
   return (
     <Fragment>
-      <AppBar position="fixed" color="primary" className={classes.appbar}>
+      <AppBar position="absolute" color="primary" className={classes.appbar}>
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
             Lost Things
@@ -52,21 +60,15 @@ const Navbar = () => {
           <Tab label="Videos" />
         </Tabs>
       </Paper>
-      <Grid container>
-        <Grid item xs={12} sm={4}>
-          <div>
-            <img src="https://images.pexels.com/photos/592753/pexels-photo-592753.jpeg?cs=srgb&dl=pexels-aaditya-arora-592753.jpg&fm=jpg" />
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <div>
-            <img src="https://images.pexels.com/photos/592753/pexels-photo-592753.jpeg?cs=srgb&dl=pexels-aaditya-arora-592753.jpg&fm=jpg" />
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <div>
-            <img src="https://images.pexels.com/photos/592753/pexels-photo-592753.jpeg?cs=srgb&dl=pexels-aaditya-arora-592753.jpg&fm=jpg" />
-          </div>
+      <Grid container justify="center" style={{ marginTop: "10px" }}>
+        <Grid item container style={{ width: "95%" }} spacing={2}>
+          {[0, 1, 2, 0, 1, 2].map((num) => {
+            return (
+              <Grid key={num} item xs={12} sm={4}>
+                <img width="100%" src={pictures[num]} />
+              </Grid>
+            );
+          })}
         </Grid>
       </Grid>
     </Fragment>
