df293075d00b6d81239a671de56ad5b0b0a75dab
ValeedAnjum <allinone644@gmail.com>
2020-12-11 12:28:23 +0500

    some refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Fragment } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./views/Home/Home";
 function App() {
   return (
-    <Fragment>
-      <Home />
-    </Fragment>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path="/h" component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -5,7 +5,6 @@ import Navbar from "../../components/Navbar/Navbar";
 const Home = () => {
   return (
     <Fragment>
-      <Navbar />
       <Items />
     </Fragment>
   );
