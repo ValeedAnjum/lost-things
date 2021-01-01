fb3699919fc7aab9a7b729b3087c16726575847b
ValeedAnjum <allinone644@gmail.com>
2021-01-01 11:57:01 +0500

    signin route

---
+++ server/routes/auth.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const User = require("../models/user");
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+
+router.post(
+  "/signin",
+  [
+    check("email", "Please enter a valid email address").isEmail(),
+    check("password", "Please enter password").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User does not exists" }] });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ errors: [{ msg: "Wrong Password" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
